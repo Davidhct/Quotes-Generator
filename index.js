1a5fc770002f689ebfd3e7b3d1183422b09c2173
Davidhct <davidhct1@gmail.com>
2021-05-16 16:53:35 +0300

    update js file

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ document.querySelector(".gen-btn").addEventListener("click", generateQuotes);
 
 function generateQuotes() {
   let rand = 0;
-  for (let i = 0; i < quotes.length; i++)
+  for (let i = 0; i < quotes.length; i++) {
     rand = Math.trunc(Math.random() * quotes.length);
-
+  }
   document.querySelector(".quote").innerHTML = `"${quotes[rand].quote}"`;
   document.querySelector(".author").innerHTML = `-${quotes[rand].author}`;
 }
